aac56c2a260e45f05c7ba7bc69a89c20c40bedb2
Ron Wexler <ronmada@gmail.com>
2021-06-06 23:36:05 +0300

    improved game tabs

--- src/app/components/game-tabs/game-tabs.component.ts
+++ src/app/components/game-tabs/game-tabs.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { Game } from 'src/app/models'
 
 @Component({
   selector: 'app-game-tabs',
   templateUrl: './game-tabs.component.html',
-  styleUrls: ['./game-tabs.component.scss']
+  styleUrls: ['./game-tabs.component.scss'],
 })
 export class GameTabsComponent implements OnInit {
-
-  constructor() { }
-
+  @Input() public game!: Game
+  public upvotes: number = 0
+  public downvotes: number = 0
+  constructor() {}
   ngOnInit(): void {
+    if (this.game) this.getGameRatings()
   }
-
+  getGameRatings(): void {
+    let upvotes: number = 0,
+      totalvotes: number = 0
+    this.game.ratings.forEach((rating) => {
+      totalvotes += rating.count
+      if (rating.title === 'exceptional' || rating.title === 'recommended')
+        upvotes += rating.count
+    })
+    this.upvotes = upvotes
+    this.downvotes = totalvotes - upvotes
+  }
 }
